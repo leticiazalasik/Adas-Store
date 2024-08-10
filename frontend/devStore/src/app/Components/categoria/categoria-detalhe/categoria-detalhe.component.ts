7ac8ea150e0d6c5c1894ac56b49fa9b8657296a5
Letícia Zalasik <leticiazalasik@gmail.com>
2024-08-10 10:35:47 -0300

    :construction: Feat (categoria-detalhe): Criação deste componente.

---
+++ frontend/devStore/src/app/Components/categoria/categoria-detalhe/categoria-detalhe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria-detalhe',
+  templateUrl: './categoria-detalhe.component.html',
+  styleUrl: './categoria-detalhe.component.css'
+})
+export class CategoriaDetalheComponent {
+
+}
