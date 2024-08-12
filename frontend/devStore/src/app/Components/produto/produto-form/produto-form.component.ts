b0ae59770251e9f4d185b1a65f96ee9169467768
Letícia Zalasik <leticiazalasik@gmail.com>
2024-08-12 17:55:49 -0300

    :sparkles: Feat (produto-form): Feito HTML do componente com um formulário e botão submit. No TS constructor, métodos ngOnInit e salvar.

--- frontend/devStore/src/app/Components/produto/produto-form/produto-form.component.ts
+++ frontend/devStore/src/app/Components/produto/produto-form/produto-form.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Produto } from '../../interfaces/produto';
+import { ProdutoService } from '../../servicos/produto.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-produto-form',
@@ -6,5 +11,40 @@ import { Component } from '@angular/core';
   styleUrl: './produto-form.component.css'
 })
 export class ProdutoFormComponent {
+produto: Produto = {
+  id: 0, 
+  nome: '',
+  preco:0, 
+  quantidade:0
+}; 
 
+isEdicao: boolean = false; 
+
+constructor(
+  private produtoService: ProdutoService, 
+  private route: ActivatedRoute, 
+  private router: Router
+){}
+
+ngOnInit(): void{
+  const id = Number(this.route.snapshot.paramMap.get('id')); 
+  if (id && id !=0){
+    this.isEdicao= true; 
+    this.produtoService.findById(id).subscribe(data => {
+      this.produto = data; 
+    }); 
+  }
+}
+
+salvar(): void {
+  if(this.isEdicao){
+    this.produtoService.update(this.produto).subscribe(() => {
+      this.router.navigate(['/produtos']); 
+    }); 
+  } else { 
+    this.produtoService.add(this.produto).subscribe(() => {
+      this.router.navigate(['/produtos']);
+    })
+  }
+  }
 }
