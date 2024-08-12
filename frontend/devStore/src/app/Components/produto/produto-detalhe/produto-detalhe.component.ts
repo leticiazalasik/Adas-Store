5a02814224528ec7566525efcf70ac7949f60949
Letícia Zalasik <leticiazalasik@gmail.com>
2024-08-12 17:23:41 -0300

    :sparkles: Feat (produto-detalhe): Feito HTML do componente com as infos do produto, um botao para editar e outro voltar. No TS feito os método ngOnInit e construtor.

--- frontend/devStore/src/app/Components/produto/produto-detalhe/produto-detalhe.component.ts
+++ frontend/devStore/src/app/Components/produto/produto-detalhe/produto-detalhe.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Produto } from '../../interfaces/produto';
+import { ActivatedRoute } from '@angular/router';
+import { ProdutoService } from '../../servicos/produto.service';
 
 @Component({
   selector: 'app-produto-detalhe',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class ProdutoDetalheComponent {
 
+  produto: Produto | undefined; 
+
+  constructor(
+    private produtoService: ProdutoService, 
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void{
+    const id = Number(this.route.snapshot.paramMap.get('id')); 
+    if (id){
+      this.produtoService.findById(id).subscribe(data=> {
+        this.produto=data; 
+      });
+    }
+  }
 }
