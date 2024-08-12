f71ef765ccd3ce929fd1e031d9caa1f18072d870
Letícia Zalasik <leticiazalasik@gmail.com>
2024-08-12 18:19:21 -0300

    :sparkles: Feat (routing.modules e app-component): feitas 5 rotas para produtos e adicionado no app-component routerLink para produto.

--- frontend/devStore/src/app/app-routing.module.ts
+++ frontend/devStore/src/app/app-routing.module.ts
@@ -3,13 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoriaListaComponent } from './Components/categoria/categoria-lista/categoria-lista.component';
 import { CategoriaDetalheComponent } from './Components/servicos/categoria-detalhe/categoria-detalhe.component';
 import { CategoriaFormComponent } from './Components/categoria/categoria-form/categoria-form.component';
+import { ProdutoListaComponent } from './Components/produto/produto-lista/produto-lista.component';
+import { ProdutoFormComponent } from './Components/produto/produto-form/produto-form.component';
+import { ProdutoDetalheComponent } from './Components/produto/produto-detalhe/produto-detalhe.component';
 
 const routes: Routes = [
   {path: 'categorias', component: CategoriaListaComponent}, 
   {path: 'categoria/nova', component:CategoriaFormComponent},
   {path: 'categoria/editar/:id', component:CategoriaFormComponent},
   {path: 'categoria/:id', component: CategoriaDetalheComponent},
-  {path: '', redirectTo: '/categorias', pathMatch: 'full'}
+  {path: '', redirectTo: '/categorias', pathMatch: 'full'},
+//produto 
+{path: 'produtos', component: ProdutoListaComponent}, 
+{path: 'produto/novo', component: ProdutoFormComponent}, 
+{path: 'produto/editar/:id', component:ProdutoFormComponent },
+{ path: 'produto/:id', component: ProdutoDetalheComponent},
+{ path: '', redirectTo: '/produtos', pathMatch:'full'}
 
 ];
 
