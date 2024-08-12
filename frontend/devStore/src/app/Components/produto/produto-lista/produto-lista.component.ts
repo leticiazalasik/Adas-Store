d7b60c3a8e13648465c2bc1af4fb478f92714488
Letícia Zalasik <leticiazalasik@gmail.com>
2024-08-12 15:56:41 -0300

    :sparkles: Feat (produto-lista): Feito HTML do componente com uma tabela responsiva e botões e feito o TS com os métodos ngOnInit, carregarProdutos, delete.

--- frontend/devStore/src/app/Components/produto/produto-lista/produto-lista.component.ts
+++ frontend/devStore/src/app/Components/produto/produto-lista/produto-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Produto } from '../../interfaces/produto';
+import { ProdutoService } from '../../servicos/produto.service';
 
 @Component({
   selector: 'app-produto-lista',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class ProdutoListaComponent {
 
+  produtos: Produto[]=[]; 
+  
+  constructor(private produtoService: ProdutoService){}; 
+
+  ngOnInit():void{
+    this.carregarProdutos();
+  }
+
+  carregarProdutos():void{
+    this.produtoService.findAll().subscribe(data=> {
+      this.produtos=data; 
+    }); 
+  }
+
+  delete(id:number):void{
+    this.produtoService.delete(id).subscribe(()=>{
+      this.carregarProdutos(); 
+    })
+  }
 }
